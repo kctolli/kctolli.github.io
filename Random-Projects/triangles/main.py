1d701d424c2b8c7119c24d8f878801cf7b4ddf91
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-02-05 15:59:53 -0700

    radians

--- Random-Projects/triangles/main.py
+++ Random-Projects/triangles/main.py
@@ -34,7 +34,8 @@ def main():
                 print("These values do not produce a valid triangle.")
             elif correct == tri.valid[1]: ## Display valid triangle type
                 print("These values produce a valid {}, {} triangle.".format(class_angle, class_side))
-                print("The angles of the triangle are {:.2f}, {:.2f}, {:.2f}.".format(tri.angle[0], tri.angle[1], tri.angle[2]))
+                print("The angles in degrees of the triangle are {:.2f}, {:.2f}, {:.2f}.".format(tri.angle[0], tri.angle[1], tri.angle[2]))                           
+                print("The angles in radians of the triangle are {:.2f}, {:.2f}, {:.2f}.".format(tri.degree2rad(tri.angle[0]), tri.degree2rad(tri.angle[1]), tri.degree2rad(tri.angle[2]))) 
         elif check == "angle" or check == "angles":
             correct, class_angle, class_side = tri.getangletriangle() ## Get triangle from sides
             if correct == tri.valid[0]: ## Display not valid triangle
