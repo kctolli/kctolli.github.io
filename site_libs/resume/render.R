b6912c0f723efaf0a4d9288cdd3c02b70fd6b45c
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-02-09 21:54:23 -0700

    render

---
+++ site_libs/resume/render.R
@@ -0,0 +1,25 @@
+# This script builds both the HTML and PDF versions of your CV
+
+# If you wanted to speed up rendering for googlesheets driven CVs you could use
+# this script to cache a version of the CV_Printer class with data already
+# loaded and load the cached version in the .Rmd instead of re-fetching it twice
+# for the HTML and PDF rendering. This exercise is left to the reader.
+
+setwd(glue::glue("{here::here()}/site_libs/resume"))
+
+file <- "resume"
+
+# Knit the HTML version
+rmarkdown::render(glue::glue("{file}.rmd"),
+                  params = list(pdf_mode = FALSE),
+                  output_file = "{file}.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("{file}.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "{file}.pdf")
