0ece1375aafa6808d5c0b3f581e00a78fcf5bf35
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-02-11 10:48:39 -0700

    data driven

--- site_libs/script.R
+++ site_libs/script.R
@@ -1,24 +1,92 @@
-print_info <- function(cv){
-  glue::glue_data(cv, "<i class='fa fa-{cv$icon}'></i> [{cv$contact}]({cv$link}) \n\n\n") %>% 
-    print()
-  invisible(cv)
+print_section <- function(cv, section_id){
+  
+  if(cv$start == cv$end){
+    
+    glue_template <- "
+### {title}
+
+{institution}
+
+{loc}
+
+{end}
+
+- {description_1}
+- {description_2}
+- {description_3}
+\n\n\n"
+  } else {
+  
+    glue_template <- "
+### {title}
+
+{institution}
+
+{loc}
+
+{start} <br> | <br> {end}
+
+- {description_1}
+- {description_2}
+- {description_3}
+\n\n\n"
+  }
+    
+  section_data <- dplyr::filter(cv, section == section_id)
+  
+  print(glue::glue_data(section_data, glue_template))
 }
 
-# print_skill_bars <- function(cv, out_of = 5, bar_color = "#969696", bar_background = "#d9d9d9", glue_template = "default"){
-#   
-#   if(glue_template == "default"){
-#     glue_template <- "
-# <div
-#   class = 'skill-bar'
-#   style = \"background:linear-gradient(to right,
-#                                       {bar_color} {width_percent}%,
-#                                       {bar_background} {width_percent}% 100%);\"
-# >{skill}</div>"
-#   }
-#   cv$skills %>%
-#     dplyr::mutate(width_percent = round(100*as.numeric(level)/out_of)) %>%
-#     glue::glue_data(glue_template) %>%
-#     print()
-#   
-#   invisible(cv)
-# }
+print_work <- function(cv){
+  
+  section_id = 'experience'
+    
+  glue_template <- "
+## {institution}
+
+### {title}
+
+{start} - {end} - {loc}
+
+- {description_1}
+- {description_2}
+- {description_3}
+\n\n\n"
+  
+  section_data <- dplyr::filter(cv, section == section_id)
+  
+  print(glue::glue_data(section_data, glue_template))
+}
+
+print_project <- function(cv){
+  
+  section_id = 'projects'
+  
+  if(cv$start == cv$end){
+    
+    glue_template <- "
+### {title}
+
+{end} - {loc}
+
+- {description_1}
+- {description_2}
+- {description_3}
+\n\n\n"
+  } else {
+    
+    glue_template <- "
+### {title}
+
+{start} - {end} - {time}
+
+- {description_1}
+- {description_2}
+- {description_3}
+\n\n\n"
+  }
+  
+  section_data <- dplyr::filter(cv, section == section_id)
+  
+  print(glue::glue_data(section_data, glue_template))
+}
