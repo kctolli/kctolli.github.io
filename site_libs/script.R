49e10057a0198dd571f61d89969329e7a0bcb6a9
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-02-22 00:41:32 -0700

    load

--- site_libs/script.R
+++ site_libs/script.R
@@ -4,6 +4,11 @@ pagebreak <- function(){pander::pander(glue::glue('<hr /> <div style="clear:both
 
 resume <- function(cv){cv %>% filter(in_resume)}
 
+load_libraries <- function(){
+  knitr::opts_chunk$set(results = 'asis', echo = FALSE, message = FALSE, warning = FALSE)
+  pacman::p_load(tidyverse, glue, pacman)
+}
+
 # Section Templates
 
 section <- function(cv, section_id, glue_template){
@@ -138,7 +143,7 @@ licence <- function(){
   template <- "
 - [Licensed](https://github.com/kctolli/kctolli.github.io/blob/master/LICENSE) under [GNU General Public License v3.0](https://github.com/kctolli/kctolli.github.io/blob/master/site_libs/GNU.txt). 
 - Website is made using [Rstudio](https://rstudio.com/) with [Rmd](https://rmarkdown.rstudio.com/index.html) and [Yaml](https://github.com/kctolli/kctolli.github.io/blob/master/_site.yml) files. 
-- Website is developed in R, HTML, CSS and Javascript. 
+- Website is developed in [R](https://github.com/kctolli/kctolli.github.io/blob/master/site_libs/script.R), HTML, CSS and [Javascript](https://github.com/kctolli/kctolli.github.io/blob/master/site_libs/script.js). 
 - Hosted on [github](https://github.com/kctolli/kctolli.github.io)."
   
   pander::pander(template)
