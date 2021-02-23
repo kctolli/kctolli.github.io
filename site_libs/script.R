f4dd217848cc98ad31a2e8057408484e880dc17b
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-02-23 15:11:29 -0700

    c()

--- site_libs/script.R
+++ site_libs/script.R
@@ -22,7 +22,7 @@ user_stats <- function(){
   header <- glue::glue("## User Stats \n\n\n")
   stats <- glue::glue('<div align="center"><img style="max-width:100%;" height="160" align="center" 
   src="https://github-readme-stats.vercel.app/api/top-langs/?username={user}&layout=compact&theme=gruvbox" /></div>')
-  pander::pander(header, stats)
+  pander::pander(c(header, stats))
 }
 
 nav <- function(){
