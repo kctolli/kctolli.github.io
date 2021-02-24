b6f72f0b84bf999879486ac67c09622820ebff03
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-02-23 22:33:30 -0700

    ds

--- site_libs/script.R
+++ site_libs/script.R
@@ -15,8 +15,16 @@ load_libraries <- function(){
 # R to HTML Functions
 
 pagebreak <- function(){pander::pander('<hr /> <div style="clear:both;"></div>')}
-
 paragraph <- function(par){pander::pander('<p>{par}</p>')}
+
+print_newline <- function(){pander::pander("\n")}
+print_h1 <- function(h1){pander::pander(glue::glue("# {h1} \n\n\n"))}
+print_h2 <- function(h2){pander::pander(glue::glue("## {h2} \n\n\n"))}
+print_h3 <- function(h3){pander::pander(glue::glue("### {h3} \n\n\n"))}
+print_h4 <- function(h4){pander::pander(glue::glue("#### {h4} \n\n\n"))}
+print_h5 <- function(h5){pander::pander(glue::glue("##### {h5} \n\n\n"))}
+print_h6 <- function(h6){pander::pander(glue::glue("###### {h6} \n\n\n"))}
+print_pic <- function(img){pander::pander(glue::glue("![]({img}) \n\n\n"))}
 
 user_stats <- function(){
   pander::pander(glue::glue('<h2>User Stats</h2>
@@ -181,16 +189,6 @@ print_highlights <- function(path){
   highlights <- read_csv(glue("{path}highlights.csv"))
   glue_data(highlights, "- [{Text}]({Link}) \n")
 }
-
-# Headers 
-
-print_h1 <- function(h1){pander::pander(glue::glue("# {h1} \n\n\n"))}
-print_h2 <- function(h2){pander::pander(glue::glue("## {h2} \n\n\n"))}
-print_h3 <- function(h3){pander::pander(glue::glue("### {h3} \n\n\n"))}
-print_h4 <- function(h4){pander::pander(glue::glue("#### {h4} \n\n\n"))}
-print_h5 <- function(h5){pander::pander(glue::glue("##### {h5} \n\n\n"))}
-print_h6 <- function(h6){pander::pander(glue::glue("###### {h6} \n\n\n"))}
-print_newline <- function(){pander::pander("\n")}
 
 # Licensing and Copyright
 
