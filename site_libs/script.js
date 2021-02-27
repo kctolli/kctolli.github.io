ce4f0f52b3a3e9db42bbbe0a7f7a79085bda34d8
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-02-26 23:27:22 -0700

    script

--- site_libs/script.js
+++ site_libs/script.js
@@ -3,9 +3,11 @@
 include("./scripts/time.js");
 include("./scripts/form.js");
 include("./scripts/await.js");
+include("./scripts/cookie.js");
+include("./scripts/screen.js");
 include("./scripts/firebase.js");
+include("./scripts/location.js");
 include("./scripts/frameworks.js");
-include("./scripts/geolocation.js");
 
 /*Basic Functions*/
 
@@ -16,5 +18,5 @@ function showhide(id) {
 
 function darkmode() {
 	var element = document.body;
-  element.classList.toggle("dark-mode");
+  	element.classList.toggle("dark-mode");
 }
