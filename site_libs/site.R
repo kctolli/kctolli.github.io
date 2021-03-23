3a7af742d04a9648ec5b8d94c5f20a4565d13c0c
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-23 14:10:27 -0600

    css to R

--- site_libs/site.R
+++ site_libs/site.R
@@ -1,12 +1,13 @@
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/js.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/css.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/html.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/render.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/gsheets.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/copyright.R")
 
 # Variables / Basic Functions
 
 here <- here::here() ## set here file path
-current_year <- lubridate::year(Sys.Date()) ## sys year
 
 load_libraries <- function(){
   pacman::p_load(tidyverse, pacman, glue, pander, lubridate, knitr, rmarkdown) ## Load Packages
@@ -172,21 +173,15 @@ nav <- function(){
   pagebreak <- pagebreak()
   showhide <- showhide()
   darkmode <- darkmode()
-  # frameworks <- frameworks()
+  yaml <- yaml_css()
+  site <- site_css()
   
   pander::pander(glue::glue('
-  {showhide}{darkmode}{pagebreak}<nav class="info"><p>How to navigate this website: </p><ul>
+  {site}{yaml}{showhide}{darkmode}{pagebreak}<nav class="info"><p>How to navigate this website: </p><ul>
   <li><span style="color:blue;">Blue</span> text - Clickable (Click to see pop up links or new pages)</li>
   <li><span style="color:gray;">Gray</span> text - Hoverable (Hover to get more information)</li></ul>
   <button class="btn" onclick="darkmode()"><i class="fas fa-adjust">Toggle Dark Mode</i></button></nav>
   {pagebreak} \n\n\n'))
-}
-
-copyright <- function(start){
-  if (start < current_year){str <- glue::glue('{start} - {current_year} -- Kyle Tolliver')}
-  else if (start == current_year) {str <- glue::glue('{current_year} -- Kyle Tolliver')}
-  else {str <- 'Time Error'}
-  pander::pander(glue::glue('&copy;{str}'))
 }
 
 footer <- function(file){
@@ -198,10 +193,51 @@ footer <- function(file){
   licence <- pander::pander(glue::glue('<ul>
   <li>[Licensed](https://github.com/kctolli/kctolli.github.io/blob/master/LICENSE) under [GNU Public License v3.0](https://github.com/kctolli/kctolli.github.io/blob/master/site_libs/GNU.txt) and hosted on [Github](https://github.com/kctolli/kctolli.github.io).
   <li>Website is made using [Rstudio](https://rstudio.com/) with [Rmd](https://raw.githubusercontent.com/kctolli/kctolli.github.io/master/{file}.Rmd) and [Yaml](https://raw.githubusercontent.com/kctolli/kctolli.github.io/master/_site.yml) files. </li>
-  <li>Website is developed in [R]({site_libs}site.R), HTML, [CSS]({site_libs}site.css) and [Javascript]({site_libs}site.js). </li>
+  <li>Website is developed in [R]({site_libs}site.R) with HTML, CSS and Javascript. </li>
   </ul>'))
 
   pander::pander(glue::glue('{pagebreak}<footer><div style="padding-left:0px;">
   <span class="tooltipr"><a href={string}><p style="color:blue;">{copyright}</p></a></span>
   <div id="copyright" style="display:none;padding-left:20px;">{licence}</div></div></footer>'))
 }
+
+site_css <- function(){
+  pander::pander('<style>
+/*
+Site Styles
+*/
+
+a {color: blue;}
+ul {text-align: left;}
+
+.info {font-size: 80%; font-style: italic; font-family: "Arial";}
+
+body {background-color: white; color: black; font-family: "Helvetica";}
+footer{clear:both; margin: auto;}
+.dark-mode {background-color: black; color: white;}
+
+hr{border-top: 1px solid black;}
+.dark-mode hr {border-top: 1px solid white;}
+
+.navbar{background-color: white;}
+.dark-mode .navbar{background-color: black;}
+
+.tocify{background-color: white; color: black; font-family: "Times";}
+
+.btn {
+  background-color: white;
+  border: none;
+  color: blue;
+  padding: 5px 10px;
+  margin: 2px;
+  border-radius: 4px;
+  cursor: pointer;   
+  font-family: "Georgia";
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+.dark-mode .btn{background-color: black;}
+.btn:hover {background-color: blue; color: white;}
+</style>')
+} 
