fc65944e4fcfb3d4b06b6acd4109085bc761473a
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-24 00:58:21 -0600

    resume

--- site_libs/site.R
+++ site_libs/site.R
@@ -1,9 +1,9 @@
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/js.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/css.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/html.R")
-source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/render.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/gsheets.R")
 source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/copyright.R")
+source("https://raw.githubusercontent.com/kctolli/scripting/main/Rscript/templates.R")
 
 # Variables / Basic Functions
 
@@ -15,66 +15,6 @@ load_libraries <- function(){
 }
 
 # Section Templates
-
-section <- function(cv, section_id, glue_template){print(glue::glue_data(filter(cv, section == section_id), glue_template))}
-
-## Glue Templates
-
-print_resume_section <- function(section_id){
-  
-  cv <- entries %>% filter(in_resume) 
-  
-  # if(cv$start %in% cv$end){
-
-    glue_template <- "
-### {title}
-
-{institution}
-
-{loc}
-
-{end}
-
-- {description_1}
-- {description_2}
-- {description_3}
-\n\n\n"
-#   } else {
-# 
-#     glue_template <- "
-# ### {title}
-# 
-# {institution}
-# 
-# {loc}
-# 
-# {start} <br> | <br> {end}
-# 
-# - {description_1}
-# - {description_2}
-# - {description_3}
-# \n\n\n"
-#   }
-
-  section(cv, section_id, glue_template)
-}
-
-print_resume_education <- function(){
-  pander::pander("
-### Brigham Young University - Idaho
-
-Bachelor of Science in Software Engineering
-
-Rexburg, ID
-
-2017 <br> | <br> 2020
-
-+ Minor: Computer Engineering and Data Science
-+ Coursework: Advanced Embedded Systems, Digital System Design, <br />Computer Architecture, Circuit Analysis, Machine Learning, 
-<br />Intermediate Stats, Data Intuition, Data Wrangling, Database Development, Applied Development, System Security, 
-Project Management, Linear Algebra, Discrete Math, Differential Calculus  
-")  
-}
 
 print_work <- function(section_id = 'experience'){
 
@@ -124,73 +64,6 @@ print_project <- function(){
   section(entries, section_id, glue_template)
 }
 
-print_tutor <- function(){
-  string <- "javascript:showhide('tutor')"
-
-  pander::pander(glue::glue('
-  ### Computer Science Tutor
-
-  April 2018 - August 2018 --- Rexburg, ID
-
-  <ul><div style="padding-left:0px;">
-  <span class="tooltipr"><a href={string}><li>Tutored many classes</li></a></span>
-  <div id="tutor" style="display:none;padding-left:20px;">
-
-    + Object-Oriented Programming with Data Structures using Python
-    + Fundamentals of Digital Systems w/ Lab
-    + Introduction to Engineering
-    + Introduction to Electrical and Computer Engineering
-
-  </div></div><li>Taught students topics from these classes</li>
-  <li>Lead group and individual appointments and discussions</li></ul>'))
-}
-
-print_ds <- function(){
-  string <- "javascript:showhide('pic')"
-  
-  pander::pander(glue::glue('
-  <div style="padding-left:0px;"><span class="tooltipr"><a href={string} align="center">
-  <img src="https://secure.meetupstatic.com/photos/event/d/7/4/b/600_482695115.jpeg" 
-  alt="Data Science Image" class="img"></a></span>
-  <h4 id="pic" style="display:none;padding-left:20px;">Interdisciplinary field that uses scientific methods, 
-  processes, algorithms and systems to extract knowledge and insights from structured and unstructured data, 
-  and apply knowledge and actionable insights from data across a broad range of application domains.</h4></div>\n\n\n'))
-}
-
-print_about <- function(){
-  # pic <- print_pic("./site_libs/images/me.jpg")
-  string <- "javascript:showhide('me')"
-  
-  pander::pander(glue::glue('## About Me \n\n 
-  BYU-Idaho graduate with a Bachelor\'s (BS) in Software Engineering. 
-  I also earned a minor in Computer Engineering and Data Science. 
-  I love to learn new things and solve problems. \n\n
-  <div style="padding-left:0px;"><span class="tooltipr"><a href={string}>
-  A little more about me</a></span><div id="me" style="display:none;padding-left:20px;">\n\n'))
-}
-
-## Basic Templates
-
-print_contact <- function(file){
-  
-  if (file == "resume"){info <- dplyr::filter(contact, in_resume)}
-  else if (file == "index"){info <- dplyr::filter(contact, in_index)}
-  else {info <- contact}
-  
-  print(glue::glue_data(info, "<i class='fa fa-{icon}'></i> [{contact}]({link}) \n\n"))
-}
-
-print_disclaimer <- function(){
-  pander::pander('Member of [Tau Beta Pi](https://tbp.org) <br /> Engineering Honor Society <br /> Idaho Delta chapter at BYU-Idaho.
-    \n\n\nThis resume was made with <br /> the R package [pagedown](https://github.com/rstudio/pagedown).')}
-
-print_pos <- function(section_id){section(pos, section_id, "- {name}\n")}
-print_solo <- function(){print(glue::glue_data(solo, "- {name} - [#{number}](https://www.sololearn.com/{link}/pdf)\n"))}
-print_skills <- function(){print(glue::glue_data(skills, "- {skill} \n"))}
-print_soc <- function(){print(glue::glue_data(society, "- {group} associated with {loc} ({start} - {end}) \n"))}
-print_highlights <- function(){print(glue::glue_data(highlights, "- [{Text}]({Link}) \n"))}
-print_portfolio <- function(){pander::pander('<p class="info">This website is setup as a personal portfolio.</p>')}
-
 # Utilities
 
 nav <- function(){
@@ -223,51 +96,3 @@ footer <- function(file){
   <span class="tooltipr"><a href={string}><p style="color:blue;">{copyright}</p></a></span>
   <div id="copyright" style="display:none;padding-left:20px;">{licence}</div></div></footer>'))
 }
-
-site_css <- function(){
-  pander::pander('<style>/*Site Styles*/
-
-a {color: blue;}
-ul {text-align: left;}
-
-.info {font-size: 80%; font-style: italic; font-family: "Arial";}
-
-body {background-color: white; color: black; font-family: "Helvetica";}
-footer{clear:both; margin: auto;}
-.dark-mode {background-color: black; color: white;}
-
-hr{border-top: 1px solid black;}
-.dark-mode hr {border-top: 1px solid white;}
-
-.navbar{background-color: white;}
-.dark-mode .navbar{background-color: black;}
-
-.tocify{background-color: white; color: black; font-family: "Times";}
-
-.img {
-  display: block; 
-  margin-left: auto; 
-  margin-right: auto; 
-  width: 125%; 
-  align: center; 
-  border-radius: 8px;
-}
-
-.btn {
-  background-color: white;
-  border: none;
-  color: blue;
-  padding: 5px 10px;
-  margin: 2px;
-  border-radius: 4px;
-  cursor: pointer;   
-  font-family: "Georgia";
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-}
-
-.dark-mode .btn{background-color: black;}
-.btn:hover {background-color: blue; color: white;}
-</style>')
-} 
